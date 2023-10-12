d3c05c1139dc9e7b2878aad05182279ad1e29328
tottywzciek29 <alejandroescudero2909@gmail.com>
2023-10-12 16:42:39 -0400

    Crear clase de personajes

--- Form/models.py
+++ Form/models.py
@@ -5,3 +5,26 @@ from django.db import models
 class FormQuestion(models.Model):
     question = models.CharField(max_length=200)
     answered = models.IntegerField(default=0)
+
+class Character(models.Model):
+    name = models.CharField(max_length=50)
+    sexuals_orientations = models.CharField(max_length=50)
+    ethnicity = models.CharField(max_length=50)
+    job = models.CharField(max_length=40)
+    migrant = models.BooleanField()
+    age = models.IntegerField()
+    country = models.CharField(max_length=15)
+    motto = models.CharField(max_length=200)
+    mbti = models.CharField(max_length=10)
+    neurodivergence = models.CharField(max_length=50)
+    disability = models.CharField(max_length=40)
+    kids = models.BooleanField()
+    identity = models.CharField(max_length=50)
+    
+
+
+
+
+
+
+
